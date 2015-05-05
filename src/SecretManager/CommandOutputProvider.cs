6aa8ab8a7d88ce7512f734f3e972d26e00ca58b9
Brennan <brecon@microsoft.com>
2015-05-05 08:54:42 -0700

    React to AnsiConsole changes

--- src/SecretManager/CommandOutputProvider.cs
+++ src/SecretManager/CommandOutputProvider.cs
@@ -2,14 +2,22 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.Framework.Logging;
+using Microsoft.Framework.Runtime;
 
 namespace SecretManager
 {
     public class CommandOutputProvider : ILoggerProvider
     {
+        private readonly bool _isWindows;
+
+        public CommandOutputProvider(IRuntimeEnvironment runtimeEnv)
+        {
+            _isWindows = runtimeEnv.OperatingSystem == "Windows";
+        }
+
         public ILogger CreateLogger(string name)
         {
-            return new CommandOutputLogger(this);
+            return new CommandOutputLogger(this, useConsoleColor: _isWindows);
         }
 
         public LogLevel LogLevel { get; set; } = LogLevel.Information;
--- test/SecretManager.Tests/ConfigurationExtensionTests.cs
+++ test/SecretManager.Tests/ConfigurationExtensionTests.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.Framework.Runtime;
+using Microsoft.Framework.Runtime.Infrastructure;
 using SecretManager;
 using SecretManager.Tests;
 using Xunit;
@@ -10,6 +12,13 @@ namespace Microsoft.Framework.ConfigurationModel.UserSecrets.Tests
 {
     public class ConfigurationExtensionTests
     {
+        private readonly IRuntimeEnvironment _runtimeEnv;
+
+        public ConfigurationExtensionTests()
+        {
+            _runtimeEnv = ((IRuntimeEnvironment)CallContextServiceLocator.Locator.ServiceProvider.GetService(typeof(IRuntimeEnvironment)));
+        }
+
         [Fact]
         public void AddUserSecrets_Does_Not_Fail_On_Non_Existing_File_Explicitly_Passed()
         {
@@ -34,8 +43,8 @@ namespace Microsoft.Framework.ConfigurationModel.UserSecrets.Tests
             string userSecretsId;
             var projectPath = UserSecretHelper.GetTempSecretProject(out userSecretsId);
 
-            var logger = new TestLogger();
-            var secretManager = new Program() { Logger = logger };
+            var logger = new TestLogger(_runtimeEnv);
+            var secretManager = new Program(_runtimeEnv) { Logger = logger };
 
             secretManager.Main(new string[] { "set", "Facebook:AppSecret", "value1", "-p", projectPath });
 
@@ -51,8 +60,8 @@ namespace Microsoft.Framework.ConfigurationModel.UserSecrets.Tests
             string userSecretsId;
             var projectPath = UserSecretHelper.GetTempSecretProject(out userSecretsId);
 
-            var logger = new TestLogger();
-            var secretManager = new Program() { Logger = logger };
+            var logger = new TestLogger(_runtimeEnv);
+            var secretManager = new Program(_runtimeEnv) { Logger = logger };
 
             secretManager.Main(new string[] { "set", "Facebook:AppSecret", "value1", "-p", projectPath });
 
